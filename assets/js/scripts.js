98e8e2483ecb32a632f26225c5c24fcd67c17c18
Petar Mitchev <pmitchev@Petars-MacBook-Air.local>
2023-12-26 14:30:09 +0200

    bug fixes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,6 +16,33 @@ var getUrlVars = function() {
   return vars;
 }
 
+var navToggled = function() {
+  if (document.getElementById("nav-list").classList.contains("active")) {
+    document.getElementById("nav-list").classList.add('inactive')
+    document.getElementById("nav-list").classList.remove('active')
+  } else {
+    document.getElementById("nav-list").classList.add('active')
+    document.getElementById("nav-list").classList.remove('inactive')
+  }
+}
+var navSearchPerformed = function() {
+  document.getElementById('navSearchButton').addEventListener("click", function() {
+    searchText = document.getElementById('navSearchText').value;
+    changeSearchRoute(searchText)
+  })
+  document.getElementById('navSearchText').addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+     event.preventDefault();
+     document.getElementById("navSearchButton").click();
+    }
+  });
+}
+var changeSearchRoute = function() {
+  if (document.getElementById('navSearchText').value) {
+    window.open('/result/?search=' + btoa(document.getElementById('navSearchText').value), "_self")
+  }
+}
+
 var lineChartHomeConfig = {
     chart: {
       type: 'line',
